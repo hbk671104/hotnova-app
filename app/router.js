e26cf6d7eed91605bb9092cdf37d09d4e5f3a64f
hbk671104 <hbk671104@gmail.com>
2018-09-15 17:56:28 +0800

    add dvajs

---
+++ app/router.js
@@ -0,0 +1,78 @@
+import * as React from 'react'
+import { BackHandler } from 'react-native'
+import {
+	createStackNavigator,
+	createBottomTabNavigator,
+	createSwitchNavigator,
+	NavigationActions,
+} from 'react-navigation'
+import {
+	reduxifyNavigator,
+	createReactNavigationReduxMiddleware,
+	createNavigationReducer,
+} from 'react-navigation-redux-helpers'
+import { connect } from 'react-redux'
+
+// Containers
+
+import Login from './container/auth/login'
+
+const AuthStack = createStackNavigator({
+	Login,
+})
+
+const AppRouter = createSwitchNavigator({
+	Auth: AuthStack,
+})
+
+export const routerReducer = createNavigationReducer(AppRouter)
+
+export const routerMiddleware = createReactNavigationReduxMiddleware(
+	'root',
+	state => state.router,
+)
+
+const App = reduxifyNavigator(AppRouter, 'root')
+
+function getActiveRouteName(navigationState) {
+	if (!navigationState) {
+		return null
+	}
+	const route = navigationState.routes[navigationState.index]
+	if (route.routes) {
+		return getActiveRouteName(route)
+	}
+	return route.routeName
+}
+
+class Router extends React.PureComponent {
+	componentWillMount() {
+		BackHandler.addEventListener('hardwareBackPress', this.backHandle)
+	}
+
+	componentWillUnmount() {
+		BackHandler.removeEventListener('hardwareBackPress', this.backHandle)
+	}
+
+	backHandle = () => {
+		const { dispatch, router } = this.props
+		const subRouter = router.routes[router.index]
+		if (subRouter.index === 0) {
+			Alert.alert('提示', '确认退出 Hotnova ？', [
+				{ text: '确认', onPress: () => BackHandler.exitApp() },
+				{ text: '取消', style: 'cancel' },
+			])
+		}
+
+		dispatch(NavigationActions.back())
+		return true
+	}
+
+	render() {
+		const { dispatch, router } = this.props
+
+		return <App dispatch={dispatch} state={router} />
+	}
+}
+
+export default connect(({ router }) => ({ router }))(Router)
--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 /** @format */
 
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
+import * as React from 'react'
+import { AppRegistry } from 'react-native'
 
-AppRegistry.registerComponent(appName, () => App);
+import { name as appName } from './app.json'
+import dva from './app/util/dva'
+import Router, { routerMiddleware, routerReducer } from './app/router'
+
+const app = dva({
+	initialState: {},
+	models: [],
+	extraReducers: { router: routerReducer },
+	onAction: [routerMiddleware],
+	onError(e) {
+		console.log('onError', e)
+	},
+})
+
+const App = app.start(<Router />)
+
+AppRegistry.registerComponent(appName, () => App)
